f5a95bcb4dbdd612246aed1ad5c1b1b6baf6ac5f
SubhashGovindharaj <subhashgovindharaj@gmail.com>
2025-03-31 11:45:17 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -16,7 +16,7 @@ def error_message_detail(error,error_detail:sys):
     return error_message  
 
 
-class Custom_exception(Exception):
+class Custom_Exception(Exception):
 
     def __init__(self,error_message,error_detail:sys):
 
